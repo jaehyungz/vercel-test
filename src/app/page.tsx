bb32582568cf2330975e7ec8e5cff3b7d5e3cf67
jaehyungz <blue259@hanmail.net>
2024-09-23 10:33:34 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>vercel 배포 해보자 hello !!!</div>;
+  return (
+    <div>vercel 배포 해보자 hello !!! 그냥 지금 자동으로 되는거아니야?</div>
+  );
 }
